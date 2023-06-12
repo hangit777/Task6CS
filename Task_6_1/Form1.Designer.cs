3676f7bc9b09a9f8b100562a80e85edb5880c78e
Nikita Moshkin <nik.moshkin.02@mail.ru>
2023-06-12 19:31:02 +0300

    Добавьте файлы проекта.

---
+++ Task_6_1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Task_6_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxClasses = new System.Windows.Forms.ComboBox();
+            this.comboBoxMethods = new System.Windows.Forms.ComboBox();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxClasses
+            // 
+            this.comboBoxClasses.FormattingEnabled = true;
+            this.comboBoxClasses.Location = new System.Drawing.Point(57, 12);
+            this.comboBoxClasses.Name = "comboBoxClasses";
+            this.comboBoxClasses.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxClasses.TabIndex = 0;
+            this.comboBoxClasses.SelectedIndexChanged += new System.EventHandler(this.comboBoxClasses_SelectedIndexChanged);
+            // 
+            // comboBoxMethods
+            // 
+            this.comboBoxMethods.FormattingEnabled = true;
+            this.comboBoxMethods.Location = new System.Drawing.Point(57, 39);
+            this.comboBoxMethods.Name = "comboBoxMethods";
+            this.comboBoxMethods.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMethods.TabIndex = 1;
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Location = new System.Drawing.Point(57, 66);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute.TabIndex = 2;
+            this.buttonExecute.Text = "Выполнить";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Класс";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Метод";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 100);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.comboBoxMethods);
+            this.Controls.Add(this.comboBoxClasses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxClasses;
+        private System.Windows.Forms.ComboBox comboBoxMethods;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Task_6_1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task_6_1;
+using System.Configuration.Assemblies;
+using ProductionClassLibrary;
+
+namespace Task_6_1
+{
+    public partial class Form1 : Form
+    {
+        private List<string> classList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            classList = ReflectionExample.GetImplementingClasses("ProductionClassLibrary.dll", typeof(IProduction));
+
+            foreach (string className in classList)
+            {
+                comboBoxClasses.Items.Add(className);
+            }
+        }
+
+        
+
+        private void buttonExecute_Click_1(object sender, EventArgs e)
+        {
+            string className = comboBoxClasses.SelectedItem.ToString();
+            Type type = Assembly.LoadFrom("ProductionClassLibrary.dll").GetType("ProductionClassLibrary." + className);
+
+
+            object obj = Activator.CreateInstance(type);
+
+            MethodInfo method = type.GetMethod(comboBoxMethods.SelectedItem.ToString());
+
+            ParameterInfo[] parameters = method.GetParameters();
+
+            string message = null;
+
+            if (parameters.Length == 0)
+            {
+                message = method.Invoke(obj, null).ToString();
+            }
+            else
+            {
+                List<object> args = new List<object>();
+
+                foreach (ParameterInfo parameter in parameters)
+                {
+                    string input = Interaction.InputBox($"Введите значение параметра {parameter.Name} для функции {method.Name}", "Значение параметра");
+                    object arg = Convert.ChangeType(input, parameter.ParameterType);
+                    args.Add(arg);
+                }
+                object  obj1 = method.Invoke(obj, args.ToArray());
+                if (obj1 != null)
+                {
+                    message = obj1.ToString();
+                }
+
+            }
+            MessageBox.Show(message ?? "Результата вывода нет");
+        }
+
+        private void comboBoxClasses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string className = comboBoxClasses.SelectedItem.ToString();
+            Type type = Assembly.LoadFrom("ProductionClassLibrary.dll").GetType("ProductionClassLibrary." + className);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+            comboBoxMethods.Items.Clear();
+            foreach (MethodInfo method in methods)
+            {
+                comboBoxMethods.Items.Add(method.Name);
+            }
+        }
+    }
+}
